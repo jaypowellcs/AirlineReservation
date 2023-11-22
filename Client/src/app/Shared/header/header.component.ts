62d6639110bc31e7a1396ba5b914b5fdd4d8014e
Jay Powell <jay.powellcs@gmail.com>
2023-11-21 19:05:02 -0600

    header,footer,home comp

---
+++ Client/src/app/Shared/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  // Initialization for ES Users
+  showMenu = false;
+
+  toggleNavbar(){
+    this.showMenu = !this.showMenu;
+  }
+}
